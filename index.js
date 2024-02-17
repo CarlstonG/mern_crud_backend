61df8069a17da09c97c1194dfd5510f6120eb23c
Carlstong <carlstongrefalde@gmail.com>
2024-02-17 18:30:55 +0800

    cors bugs

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const UserModel = require('./models/User')
 
 const app = express()
 app.use(cors({
-    origin: 'https://mern-crud-frontend-delta.vercel.app/',
+    origin: 'https://mern-crud-frontend-delta.vercel.app',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
 }))
